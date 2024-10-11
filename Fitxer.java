31f92e81cfff5928256e135cb5ee49d159a0a480
El teu nomMiguelvera17 <2023_miguel.vera@iticbcn.cat>
2024-10-11 12:57:22 +0200

    comments

--- Fitxer.java
+++ Fitxer.java
@@ -10,12 +10,12 @@ public class Fitxer {
         try (BufferedWriter line = new BufferedWriter(new FileWriter(file))) {
             line.write("Client's name:  " + client.getName() + "\n" +
             "Client's phone: " + client.getPhone() + "\n" +
-            "Order's date:   " + client.getDate() + "\n" +
+            "Order's date:   " + client.getDate() + "\n" +                    // Write the client's information  
             "\nQuantity       Units     Article\n"  +
             "============= ========== ===========");
-            for (int i = 0; i < articles.size(); i++) {                        // Read all the articles of the client
-                a = articles.get(i).toString();
-                line.write(a);
+            for (int i = 0; i < articles.size(); i++) {                       // Read all the articles of the client
+                a = articles.get(i).toString(); 
+                line.write(a);                                                // Write the list of articles
             }
             line.close();
             System.out.println("\nDocument created successfully");          // The document is created
@@ -33,9 +33,9 @@ public class Fitxer {
             line.write(client.getName() + ";" + client.getPhone() + ";" + client.getDate() + ";");      // Write the client's information
             for (int i = 0; i < articles.size(); i++) {                       // Read all the articles of the client
                 if (i == articles.size()-1) {
-                    a = a + Article.toCSV(articles.get(i));
+                    a = a + Article.toCSV(articles.get(i));                   // Accumulate
                 } else {
-                    a = a + Article.toCSV(articles.get(i)) + ";";
+                    a = a + Article.toCSV(articles.get(i)) + ";";             // Accumulate 
                 }
             }
             line.write(a);                                                    // Write the line
@@ -54,19 +54,19 @@ public class Fitxer {
         try (FileOutputStream fileStr1 = new FileOutputStream(file)) {
             DataOutputStream line = new DataOutputStream(fileStr1);
             line.writeUTF(client.getName());                                // Write the name of the client (String)
-            line.writeUTF(client.getPhone());                               // Write the name of the client (String)
-            line.writeUTF(client.getDate());                                // Write the line
+            line.writeUTF(client.getPhone());                               // Write the phone of the client (String)
+            line.writeUTF(client.getDate());                                // Write the date (String)
             for (int i = 0; i < articles.size(); i++) {
                 Article art = articles.get(i);
-                line.writeFloat(art.getQuantity());
-                line.writeUTF(art.getUnit());
-                line.writeUTF(art.getName());
+                line.writeFloat(art.getQuantity());                         // Write the quantity (Float)
+                line.writeUTF(art.getUnit());                               // Write the units (String)
+                line.writeUTF(art.getName());                               // Write the name of the article (String)
             }
             line.close();
             fileStr1.close();
-            System.out.println("\nDocument created successfully");
+            System.out.println("\nDocument created successfully");      // The document is created
         } catch (FileNotFoundException e) {
-            System.out.println("\nFAIL, documento no created");
+            System.out.println("\nFAIL, documento no created");         // The document isn't created
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -74,25 +74,25 @@ public class Fitxer {
 
     public static void readBinari() {
         // Ask for the path of the document you want to read
-        System.out.print("\nIndicate the path: ");
+        System.out.print("\nIndicate the path: ");                           // Ask for the path
         String filePath = Entrada.readLine();
         try (FileInputStream f1 = new FileInputStream(filePath)) {
             DataInputStream f = new DataInputStream(f1);
-            System.out.println("\nClient's name: " + f.readUTF());
-            System.out.println("Client's phone: " + f.readUTF());
-            System.out.println("Order's date: " + f.readUTF());
+            System.out.println("\nClient's name: " + f.readUTF());              // Print name of the client (String)
+            System.out.println("Client's phone: " + f.readUTF());               // Print phone of the client (String)
+            System.out.println("Order's date: " + f.readUTF());                 // Print date (String)
             System.out.println("\nQuantity       Units     Article\n" + 
                                     "============= ========== ===========");
 
-            System.out.println(String.format(Locale.US, "%-13.1f %-10s %s", f.readFloat(), f.readUTF(), f.readUTF()));
+            System.out.println(String.format(Locale.US, "%-13.1f %-10s %s", f.readFloat(), f.readUTF(), f.readUTF()));  // Print the list of article (Flaot, String, String)
             f.close();
             f1.close();
         } catch (FileNotFoundException e) {
-            System.out.println("\nFAIL, path not correct");
+            System.out.println("\nFAIL, path not correct");         // The document is not found
             
         } 
         catch (EOFException e) {
-            System.out.println("\nFAIL, incorrect document");  
+            System.out.println("\nFAIL, incorrect document");       // Not correct extension
         }
         catch (IOException e) {
                 e.printStackTrace();
@@ -101,9 +101,9 @@ public class Fitxer {
 
     public static void readCSV () {
         // Ask for the path of the document you want to read
-        System.out.print("\nIndicate the path: ");
+        System.out.print("\nIndicate the path: ");                          // Ask for the path
         String filePath = Entrada.readLine();
-        if (filePath.endsWith(".csv")) {
+        if (filePath.endsWith(".csv")) {                               // Check the suffix
             try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
                 String line;
 
@@ -115,29 +115,29 @@ public class Fitxer {
                     String clientName = values[0];
                     String clientPhone = values[1];
                     String orderDate = values[2];
-                    System.out.println("\nClient's name:  " + clientName);
-                    System.out.println("Client's phone: " + clientPhone);
-                    System.out.println("Order's date:   " + orderDate);
+                    System.out.println("\nClient's name:  " + clientName);      // Print name of the client (String)
+                    System.out.println("Client's phone: " + clientPhone);       // Print phone of the client (String)
+                    System.out.println("Order's date:   " + orderDate);         // Print date (String)
                     System.out.println(String.format("%-12s %-10s %-12s", "Quantity", "Units", "Article"));
                     System.out.println(String.format("=========== ========== ==========="));
                     for (int i = 3; i < values.length; i += 3) {
                         String article = values[i];
                         String quantity = values[i + 1];
                         String units = values[i + 2];
-                        System.out.println(String.format(Locale.US, "%-12s %-10s %-12s", quantity, units, article));
+                        System.out.println(String.format(Locale.US, "%-12s %-10s %-12s", quantity, units, article));    // Print the list of article (Flaot, String, String)
                     }
                 }
             } catch (FileNotFoundException e) {
-                System.out.println("\nFAIL, path not correct");
+                System.out.println("\nFAIL, path not correct");         // The document is not found
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
-            System.out.println("\nFAIL, incorrect document");
+            System.out.println("\nFAIL, incorrect document");           // Not correct extension
         }
     }
 
-    public static void selectDocument(ArrayList<Article> articles , Client client) throws IOException{
+    public static void selectDocument(ArrayList<Article> articles , Client client) throws IOException{      // Method to create a document
         while (true) {
             System.out.println("\nWhich document do you want?:\n" + 
                             "a) Albaran  [a] \n" + 
@@ -145,17 +145,17 @@ public class Fitxer {
                             "c) CSV \t    [c] \n");
             System.out.print("----> ");
             String select = Entrada.readLine();
-            if (select.equals("a")) {
+            if (select.equals("a")) {                           // to create a delivery note
                 createAlbaran(articles, client);
                 
                 break;
             }
-            if (select.equals("b")) {
+            if (select.equals("b")) {                           // to create a binary file
                 Fitxer.createBinari(articles, client);
                 
                 break;
             }
-            if (select.equals("c")) {
+            if (select.equals("c")) {                           // to create a .csv file
                 Fitxer.createCSV(articles, client);
 
                 break;
@@ -168,7 +168,7 @@ public class Fitxer {
         }
     }
 
-    public static void selectReadDocument() throws IOException {
+    public static void selectReadDocument() throws IOException {            // Method to choose which document you want to read
         System.out.println("\nWhich document do you want read?:\n" +  
                                     "a) Binary   [b]\n" +
                                     "b) CSV \t    [c] \n" +
@@ -176,13 +176,13 @@ public class Fitxer {
         System.out.print("----> ");
         String ans = Entrada.readLine();
                 
-        if (ans.equals("b")) {
+        if (ans.equals("b")) {                                      // [b] to read a binary file
             Fitxer.readBinari();
         }
-        if (ans.equals("c")) {
+        if (ans.equals("c")) {                                      // [c] to read a .CSV file
             Fitxer.readCSV();
         }
-        if (ans.equals("x")) {
+        if (ans.equals("x")) {                                      // [x] to exit
         }
         if (!ans.equals("b") && !ans.equals("c") && !ans.equals("x")) {
             System.out.println("\n=======================");
--- README.md
+++ README.md
@@ -26,6 +26,43 @@ g) Data Retrieval: Read previously saved orders from files.
     └── Store.java            # Main class for the store application
     └── README.md             # Project documentation
 ````   
+Store.java
+
+- Variables estáticas:
+
+1. public static Article article: Representa un artículo en la tienda.
+2. public static int num: Cuenta el número de artículos creados (inicia en 1).
+
+- Método main(String[] args): El flujo principal se gestiona aquí. Presenta un menú que permite al usuario elegir entre las siguientes opciones:
+
+n) Crear un nuevo pedido.
+s) Mostrar un pedido anterior.
+x) Salir del sistema.
+
+- The menu is shown when the program is launched and runs in a while loop until the user enters the exit option (x).
+
+- Generate a new order (n):
+
+1. Calls Client.createClient() to create a new client.
+2. Calls Article.createArticle(client) to generate a list of articles associated with the client.
+3. Finally, Fitxer.selectDocument(articles, client) allows the user to select the type of document they want to create (invoice, delivery note, etc.).
+
+- Show previous order (s):
+
+1. Calls Fitxer.selectReadDocument() to allow the user to select and view a previously saved order document.
+2. Input validation: If the user enters an invalid option, an error message is displayed, prompting them to try again.
+
+- Client: Responsible for creating and managing client instances. Provides static methods to initialize clients.
+
+- Article: Manages the creation of articles associated with a client.
+    createArticle(Client client) generates a list of articles for a given client.
+- Fitxer: Handles file operations such as creating and reading documents.
+
+    selectDocument(ArrayList<Article> articles, Client client) allows the creation of a document based on the articles and client.
+    selectReadDocument() loads a previously saved document for review.
+    
+- Entrada: Handles console input. It uses a BufferedReader to read user input from the console (System.in).
+
 
 # 📖 Usage
 Step 1: Client Information
